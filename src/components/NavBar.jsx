db13b40c5e49ed3434c1f6936455d1ce4759e562
Arnon <arno.l.rider@gmail.com>
2026-02-13 23:40:47 +0700

    edit word

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,13 +4,13 @@ function NavBar() {
   return (
     <div className=" bg-blue-400 h-10 flex text-3xl text-white ">
       <div className={" flex justify-center hover:bg-blue-600 w-30 "}>
-        <NavLink to="/">Lonig</NavLink>
+        <NavLink to="/">Login</NavLink>
       </div>
       <div className={"  flex justify-center hover:bg-blue-600 w-30"}>
         <NavLink to="register">Register</NavLink>
       </div>
       <div className={"flex justify-center hover:bg-blue-600 w-30"}>
-        <NavLink to="todos">Todos</NavLink>
+        <NavLink to="todos">ToDoS</NavLink>
       </div>
     </div>
   );
