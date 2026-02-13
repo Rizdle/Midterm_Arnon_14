cac4fcabd3b2d0b1eaffc34f0b5f6273be3fd88b
Gon_Ar <arno.l.rider@gmail.com>
2026-02-13 17:59:59 +0700

    update_1

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,13 +3,13 @@ import { NavLink } from "react-router";
 function NavBar() {
   return (
     <div className=" bg-blue-400 h-10 flex text-3xl text-white ">
-      <div className={"bg-blue-600 w-30  flex justify-center"}>
+      <div className={" flex justify-center hover:bg-blue-600 w-30 "}>
         <NavLink to="/">Lonig</NavLink>
       </div>
-      <div className={"bg-blue-600 w-30  flex justify-center"}>
+      <div className={"  flex justify-center hover:bg-blue-600 w-30"}>
         <NavLink to="register">register</NavLink>
       </div>
-      <div className={"bg-blue-600 w-30   flex justify-center"}>
+      <div className={"flex justify-center hover:bg-blue-600 w-30"}>
         <NavLink to="todos">Todos</NavLink>
       </div>
     </div>
